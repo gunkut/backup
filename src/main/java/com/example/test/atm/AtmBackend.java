9ca47f184eefd96c08857932cb9336316306f79a
ggunkut@outlook.com <ggunkut@outlook.com>
2022-07-07 17:25:26 +0300

    last changes

--- src/main/java/com/example/test/atm/AtmBackend.java
+++ src/main/java/com/example/test/atm/AtmBackend.java
@@ -8,12 +8,12 @@ import org.hibernate.cfg.Configuration;
 
 public class AtmBackend {
 
-    public static int twoHundredAmount;
-    public static int oneHundredAmount;
-    public static int fiftyAmount;
-    public static int twentyAmount;
-    public static int tenAmount;
-    public static int fiveAmount;
+    public int twoHundredAmount;
+    public int oneHundredAmount;
+    public int fiftyAmount;
+    public int twentyAmount;
+    public int tenAmount;
+    public int fiveAmount;
     private double balance;
     /*private double deposit;
     private double withdraw;*/
@@ -29,7 +29,7 @@ public class AtmBackend {
         session.beginTransaction();
     }
     public double getBalance() {
-        double money = 0;
+        int money = 0;
         try {
             connectDatabase();
             Atm atm = session.get(Atm.class, 1);
@@ -56,74 +56,162 @@ public class AtmBackend {
         this.balance = balance;
     }
 
-    public int checkTwoHundred(int money){
+    /*public int parse(int money, int currency, String op){
         Atm atm = session.get(Atm.class, 1);
-        if(atm.getTwo_hundred() != 0  && atm.getTwo_hundred() >= money / 200) {
-            twoHundredAmount = money / 200;
-            money %= 200;
-
+        int amount = money / currency;
+        if (op.equals("-")){
+            if(amount != 0 && atm.getTwo_hundred() >= amount) {
+                System.out.println("200 is working");
+                twoHundredAmount = amount;
+                money %= 200;
+            }
             return money;
         } else {
+            if(amount != 0) {
+                twoHundredAmount = amount;
+                money %= 200;
+            }
+        }
+
+        return money;
+    }*/
+    public int parseTwoHundred(int money, String op, Atm atm){
+        System.out.println("200 is checking");
+        int amount = money / 200;
+        if (op.equals("-")){
+            if(amount != 0 && atm.getTwo_hundred() >= amount) {
+                System.out.println("200 is working");
+                twoHundredAmount = amount;
+                money %= 200;
+
+                return money;
+            }
+            return money;
+        } else {
+            if(amount != 0) {
+                twoHundredAmount = amount;
+                money %= 200;
+
+                return money;
+            }
             return money;
         }
     }
 
-    public int checkOneHundred(int money){
-        Atm atm = session.get(Atm.class, 1);
-        if(atm.getOne_hundred() != 0  && atm.getOne_hundred() >= money / 200) {
-            oneHundredAmount = money / 100;
-            money %= 100;
+    public int parseOneHundred(int money, String op, Atm atm){
+        System.out.println("100 is checking");
+        System.out.println(atm.getOne_hundred());
+        int amount = money / 100;
+        if (op.equals("-")){
+            if(amount != 0 && atm.getOne_hundred() >= amount) {
+                System.out.println("100 is working");
+                oneHundredAmount = amount;
+                money %= 100;
 
+                return money;
+            }
             return money;
         } else {
+            if(amount != 0) {
+                oneHundredAmount = amount;
+                money %= 100;
+
+                return money;
+            }
             return money;
         }
     }
 
-    public int checkFifty(int money){
-        Atm atm = session.get(Atm.class, 1);
-        if(atm.getFifty() != 0  && atm.getFifty() >= money / 50) {
-            fiftyAmount = money / 50;
-            money %= 50;
+    public int parseFifty(int money, String op, Atm atm){
+        System.out.println("50 is checking");
+        System.out.println(atm.getFifty());
+        int amount = money / 50;
+        if (op.equals("-")){
+            if(amount != 0 && atm.getFifty() >= amount) {
+                System.out.println("50 is working");
+                fiftyAmount = amount;
+                money %= 50;
 
+                return money;
+            }
             return money;
         } else {
+            if(amount != 0) {
+                fiftyAmount = amount;
+                money %= 50;
+
+                return money;
+            }
             return money;
         }
     }
 
-    public int checkTwenty(int money){
-        Atm atm = session.get(Atm.class, 1);
-        if(atm.getTwenty() != 0  && atm.getTwenty() >= money / 20) {
-            twentyAmount = money / 20;
-            money %= 20;
+    public int parseTwenty(int money, String op, Atm atm){
+        System.out.println("20 is checking");
+        System.out.println(atm.getTwenty());
+        int amount = money / 20;
+        if (op.equals("-")){
+            if(amount != 0 && atm.getTwenty() >= amount) {
+                System.out.println("20 is working");
+                twentyAmount = amount;
+                money %= 20;
 
+                return money;
+            }
             return money;
         } else {
+            if(amount != 0) {
+                twentyAmount = amount;
+                money %= 20;
+
+                return money;
+            }
             return money;
         }
     }
 
-    public int checkTen(int money){
-        Atm atm = session.get(Atm.class, 1);
-        if(atm.getTen() != 0  && atm.getTen() >= money / 10) {
-            tenAmount = money / 10;
-            money %= 10;
+    public int parseTen(int money, String op, Atm atm){
+        System.out.println("10 is checking");
+        System.out.println(atm.getTen());
+        int amount = money / 10;
+        if (op.equals("-")){
+            if(amount != 0 && atm.getTen() >= amount) {
+                tenAmount = amount;
+                money %= 10;
 
+                return money;
+            }
             return money;
         } else {
+            if(amount != 0) {
+                tenAmount = amount;
+                money %= 10;
+
+                return money;
+            }
             return money;
         }
     }
 
-    public int checkFive(int money){
-        Atm atm = session.get(Atm.class, 1);
-        if(atm.getFive() != 0  && atm.getFive() >= money / 5) {
-            fiveAmount = money / 5;
-            money %= 5;
+    public int parseFive(int money, String op, Atm atm){
+        System.out.println("5 is checking");
+        System.out.println(atm.getFive());
+        int amount = money / 5;
+        if (op.equals("-")){
+            if(amount != 0 && atm.getFive() >= amount) {
+                fiveAmount = amount;
+                money %= 5;
 
+                return money;
+            }
             return money;
         } else {
+            if(amount != 0) {
+                fiveAmount = amount;
+                money %= 5;
+
+                return money;
+            }
             return money;
         }
     }
@@ -135,39 +223,61 @@ public class AtmBackend {
             connectDatabase();
             Atm atm = session.get(Atm.class, 1);
             if (op.equals("-")) {
-                System.out.println("c is minus");
+                System.out.println("op is minus");
                 if (money <= getBalance()) {
-
-                    checkTwoHundred(money);
-                    checkOneHundred(checkTwoHundred(money));
-                    checkFifty(checkOneHundred(money));
-                    checkTwenty(checkFifty(money));
-                    checkTen(checkTwenty(money));
-                    checkFive(checkTen(money));
-
-                    atm.setTwo_hundred(atm.getTwo_hundred() - twoHundredAmount );
-                    atm.setOne_hundred(atm.getTwo_hundred() - oneHundredAmount );
-                    atm.setFifty(atm.getTwo_hundred() - fiftyAmount );
-                    atm.setTwenty(atm.getTwo_hundred() - twentyAmount );
-                    atm.setTen(atm.getTwo_hundred() - tenAmount );
-                    atm.setFive(atm.getTwo_hundred() - fiveAmount );
+                    System.out.println("requested money is available");
+                    money = parseTwoHundred(money, "-", atm);
+                    if (atm.getTwo_hundred() >= twoHundredAmount) {
+                        System.out.println(twoHundredAmount);
+                        atm.setTwo_hundred(atm.getTwo_hundred() - twoHundredAmount );
+                    }
+                    money = parseOneHundred(money, "-", atm);
+                    if (atm.getOne_hundred() >= oneHundredAmount) {
+                        System.out.println("adet:" + oneHundredAmount);
+                        atm.setOne_hundred(atm.getOne_hundred() - oneHundredAmount );
+                    }
+                    money = parseFifty(money, "-", atm);
+                    if (atm.getFifty() >= fiftyAmount) {
+                        System.out.println("adet:" + fiftyAmount);
+                        atm.setFifty(atm.getFifty() - fiftyAmount );
+                    }
+                    money = parseTwenty(money, "-", atm);
+                    if (atm.getTwenty() >= twentyAmount) {
+                        System.out.println("adet:" + twentyAmount);
+                        atm.setTwenty(atm.getTwenty() - twentyAmount );
+                    }
+                    money = parseTen(money, "-", atm);
+                    if (atm.getTen() >= tenAmount) {
+                        System.out.println("adet:" + tenAmount);
+                        atm.setTen(atm.getTen() - tenAmount );
+                    }
+                    parseFive(money ,"-", atm);
+                    if (atm.getFive() >= fiveAmount) {
+                        System.out.println("adet:" + fiveAmount);
+                        atm.setFive(atm.getFive() - fiveAmount );
+                    }
                 } else {
                     System.out.println("Insufficient Balance");
                 }
             } else {
-                if  (money == 5) {
-                    atm.setFive(atm.getFive() + 1);
-                } else if (money == 10) {
-                    atm.setTen(atm.getTen() + 1);
-                } else if (money == 20) {
-                    atm.setTwenty(atm.getTwenty() + 1);
-                } else if (money == 50) {
-                    atm.setFifty(atm.getFifty() + 1);
-                } else if (money == 100) {
-                    atm.setOne_hundred(atm.getOne_hundred() + 1);
-                } else if (money == 200) {
-                    atm.setTwo_hundred(atm.getTwo_hundred() + 1);
-                }
+                System.out.println("op is positive");
+                money = parseTwoHundred(money, "+", atm);
+                atm.setTwo_hundred(atm.getTwo_hundred() + twoHundredAmount);
+
+                money = parseOneHundred(money, "+", atm);
+                atm.setOne_hundred(atm.getOne_hundred() + oneHundredAmount);
+
+                money = parseFifty(money, "+", atm);
+                atm.setFifty(atm.getFifty() + fiftyAmount);
+
+                money = parseTwenty(money, "+", atm);
+                atm.setTwenty(atm.getTwenty() + twentyAmount);
+
+                money = parseTen(money, "+", atm);
+                atm.setTen(atm.getTen() + tenAmount);
+
+                parseFive(money, "+", atm);
+                atm.setFive(atm.getFive() + fiveAmount);
             }
             session.getTransaction().commit();
             System.out.println("Done!!");
